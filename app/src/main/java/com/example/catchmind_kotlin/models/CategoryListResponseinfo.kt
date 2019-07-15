ff98d8054293d1116bcc374fec0c3b321f9a169e
dksush0828 <dksush0828@hanmail.net>
2019-07-15 13:02:02 +0900

    .

---
+++ app/src/main/java/com/example/catchmind_kotlin/models/CategoryListResponseinfo.kt
@@ -0,0 +1,10 @@
+package com.example.catchmind_kotlin.models
+
+import com.google.gson.annotations.SerializedName
+
+class CategoryListResponseinfo {
+
+    var id : String? = null
+    var pw : String? = null
+
+}
